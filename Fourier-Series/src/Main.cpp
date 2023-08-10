13647556b5738fd2dab90d51df931529a4c34e89
C42xe2 <erikdonath2006@gmail.com>
2023-08-10 20:29:51 +0200

    Added Gui.
    Added Gui with Raygui.

--- Fourier-Series/src/Main.cpp
+++ Fourier-Series/src/Main.cpp
@@ -1,5 +1,6 @@
 #include "raylib.h"
 #include "raymath.h"
+#include "../vendor/raygui.h"
 
 #include "Defines.h"
 
@@ -11,17 +12,25 @@
 #define PointerColor RED
 #define LineColor ORANGE
 
-#define LineSec 315
+#define LineSec 210
 float line[LineSec];
 double nextPush = -666.0f;
 
+float g_Speed = 5.0f;
+float g_N = 10;
+
+void ClearLine(float y) {
+	for (u32 i = 0; i < LineSec - 1; i++) {
+		line[i] = y;
+	}
+}
 void PushLine(float y) {
 	for(u32 i = LineSec - 1; i > 0; i--) {
 		line[i] = line[i - 1];
 	}
 	line[0] = y;
 }
-void DrawSeries(Vector2 start, u32 n = 100, float speed = 1.0f) {
+void DrawSeries(Vector2 start, float lineX, u32 n = 100, float speed = 1.0f) {
 	double t = GetTime();
 	for (u32 i = 0; i < n; i++) {
 		i32 n = 2 * i + 1; // N function
@@ -38,40 +47,59 @@ void DrawSeries(Vector2 start, u32 n = 100, float speed = 1.0f) {
 	}
 
 	// Draw the line
-	float px = Width / 2 + Width / 10;
 	for (u32 i = 0; i < LineSec - 2; i++) {
-		u32 s = 2 * i + px;
+		u32 s = 2 * i + lineX;
 		DrawLine(s, line[i], s + 2, line[i + 1], LineColor);
 	}
 
 	// Draw Pointer
-	Vector2 pv{ px, start.y };
-	DrawLineV(start, pv, PointerColor);
+	Vector2 pv{ lineX, start.y };
 	DrawLineV(pv, { pv.x, line[0] }, LineColor);
+	DrawLineV(start, pv, PointerColor);
 
 	if (t >= nextPush) {
 		PushLine(start.y);
 		nextPush = t + 0.025 * (1.0f / speed);
 	}
 }
+void DrawGui(float height) {
+	DrawRectangle(0, height, Width, Height - height, { 20, 20, 20, 255 });
+	GuiSliderBar({ 50, height + 20, 200, 20 }, "Speed", TextFormat("%0.2f", g_Speed), &g_Speed, 0.01f, 50.0f);
+	GuiSliderBar({ 50, height + 50, 200, 20 }, "N", TextFormat("%0.0f", g_N), &g_N, 0.0f, 100.0f);
+	DrawText(TextFormat("%0.3fsec", nextPush - GetTime()), 285+240, height + 30, 30, GRAY);
+	if (GuiButton({ 285+0, height + 20, 50,  50 }, "Reset")) {
+		g_N = 10.0f;
+		g_Speed = 5.0f;
+		nextPush = -666.0f;
+		ClearLine(line[0]);
+	}
+	if (GuiButton({ 285+60, height + 20, 50,  50 }, "Ultra")) {
+		g_N = 100.0f;
+		g_Speed = 25.0f;
+		nextPush = -666.0f;
+	}
+	if (GuiButton({ 285+120, height + 20, 50,  50 }, "Clear")) {
+		nextPush = -666.0f;
+		ClearLine(line[0]);
+	}
+	if (GuiButton({ 285+180, height + 20, 50,  50 }, "Push")) {
+		nextPush = -666.0f;
+	}
+}
 
 int main(i32 argc, char* argv[]) {
 	//SetConfigFlags(FLAG_VSYNC_HINT);
-	InitWindow(1500, 800, "Fourier Series");
+	InitWindow(1000, 600, "Fourier Series");
 
 	// Init line to center
-	for (u32 i = 0; i < LineSec - 1; i++) {
-		line[i] = Height / 3;
-	}
+	ClearLine(250);
 
 	while (!WindowShouldClose()) {
 		BeginDrawing();
-		ClearBackground({ 35, 35, 35, 255 });
+		ClearBackground({ 30, 30, 30, 255 });
 
-		DrawSeries({ Width / 3.0f, Height / 3.0f }, 50, 8.0f); // speed = 0.4462278f
-		
-		i32 height = Height / 2 + Height / 10;
-		DrawRectangle(0, height, Width, Height - height, { 30, 30, 30, 255 });
+		DrawSeries({ 300, 250 }, 600, floor(g_N), g_Speed); // speed = 0.4462278f
+		DrawGui(515);
 
 		DrawFPS(5, 5);
 		EndDrawing();
---
+++ Fourier-Series/vendor/raygui.c
@@ -0,0 +1,3 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define RAYGUI_IMPLEMENTATION
+#include "raygui.h"
